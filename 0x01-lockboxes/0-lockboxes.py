add45a21281c779d7304c6428a72d642375b6052
AYcrown77 <aycrown77@gmail.com>
2023-04-02 14:34:09 +0000

    Solve the task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pytho3
+"""Method to determine if all boxes can be opened"""
+
+
+def canUnlockAll(boxes):
+    """Method to determine if all boxes can be opened
+
+    args:
+        boxes: list of lists
+
+    """
+    keys = [0]
+    for key in keys:
+        for key in boxes[key]:
+            if key not in keys and key < len(boxes):
+                keys.append(key)
+    if len(keys) == len(boxes):
+        return True
+    return False
