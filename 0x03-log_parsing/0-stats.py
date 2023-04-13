7af16f9a14aa01c982bde218230917d86f81fc5c
AYcrown77 <aycrown77@gmail.com>
2023-04-13 12:20:10 +0000

    Fix the error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,44 @@
 #!/usr/bin/python3
-"""Log Analyzer"""
+"""Log Parser"""
 import sys
 
 
 if __name__ == '__main__':
-    file_sizes = [0]
+    file_size = [0]
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
 
-    def print_statistics():
-        """Print the statistics"""
-        print('File size: {}'.format(file_sizes[0]))
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
         for key in sorted(status_codes.keys()):
             if status_codes[key]:
                 print('{}: {}'.format(key, status_codes[key]))
 
-    def parse_log_line(line):
-        """Check each log line"""
+    def parse_line(line):
+        """ Checks the line for matches """
         try:
             line = line[:-1]
-            words = line.split(' ')
-            # File size is the last parameter in the log line
-            file_sizes[0] += int(words[-1])
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
             # Status code comes before file size
-            status_code = int(words[-2])
+            status_code = int(word[-2])
             # Move through dictionary of status codes
             if status_code in status_codes:
                 status_codes[status_code] += 1
         except BaseException:
             pass
 
-    line_number = 1
+    linenum = 1
     try:
         for line in sys.stdin:
-            parse_log_line(line)
-            """Print statistics after every 10 log lines"""
-            if line_number % 10 == 0:
-                print_statistics()
-            line_number += 1
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_statistics()
+        print_stats()
         raise
-    print_statistics()
+    print_stats()
