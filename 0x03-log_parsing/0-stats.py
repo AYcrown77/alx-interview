ba45ea888682e29dd9648cb5f81f5536a25afa51
AYcrown77 <aycrown77@gmail.com>
2023-04-13 12:18:10 +0000

    Fix the error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
-"""Log Parser function"""
+"""Log Analyzer"""
 import sys
 
 
 if __name__ == '__main__':
-    size = [0]
+    file_sizes = [0]
     status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
                     403: 0, 404: 0, 405: 0, 500: 0}
 
-    def stats_output():
-        """ Function that prints the stats """
-        print('File size: {}'.format(size[0]))
+    def print_statistics():
+        """Print the statistics"""
+        print('File size: {}'.format(file_sizes[0]))
         for key in sorted(status_codes.keys()):
             if status_codes[key]:
                 print('{}: {}'.format(key, status_codes[key]))
 
-    def parse_line(line):
-        """ Function that checks the line for matches """
+    def parse_log_line(line):
+        """Check each log line"""
         try:
             line = line[:-1]
-            word = line.split(' ')
-            size[0] += int(word[-1])
-            status_code = int(word[-2])
+            words = line.split(' ')
+            # File size is the last parameter in the log line
+            file_sizes[0] += int(words[-1])
+            # Status code comes before file size
+            status_code = int(words[-2])
+            # Move through dictionary of status codes
             if status_code in status_codes:
                 status_codes[status_code] += 1
         except BaseException:
             pass
 
-    line_num = 1
+    line_number = 1
     try:
         for line in sys.stdin:
-            parse_line(line)
-            if line_num % 10 == 0:
-                print_stats()
-            line_num += 1
+            parse_log_line(line)
+            """Print statistics after every 10 log lines"""
+            if line_number % 10 == 0:
+                print_statistics()
+            line_number += 1
     except KeyboardInterrupt:
-        stats_output()
+        print_statistics()
         raise
-    stats_output()
+    print_statistics()
