64eddba42f4a370b4890d86d43d5a8af49bda867
AYcrown77 <aycrown77@gmail.com>
2023-04-13 12:40:47 +0000

    Make the file executable

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,44 +1,45 @@
 #!/usr/bin/python3
 """
-This script reads standard input line by line and calculates metrics.
+Reads stdin line by line and computes metrics
 """
 
 from sys import stdin
 
 if __name__ == "__main__":
     total_size = 0
-    status_counts = {}
-    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    for code in status_codes:
-        status_counts[code] = 0
+    status_codes = {}
+    list_status_codes = [
+        "200", "301", "400", "401", "403", "404", "405", "500"]
+    for status in list_status_codes:
+        status_codes[status] = 0
     count = 0
     try:
         for line in stdin:
             try:
-                fields = line.split(" ")
-                if len(fields) != 9:
-                    continue
-                status_code = fields[-2]
-                if status_code in status_codes:
-                    status_counts[status_code] += 1
-                if fields[-1][-1] == '\n':
-                    fields[-1] = fields[-1][:-1]
-                total_size += int(fields[-1])
-            except Exception:
+                args = line.split(" ")
+                if len(args) != 9:
+                    pass
+                if args[-2] in list_status_codes:
+                    status_codes[args[-2]] += 1
+                if args[-1][-1] == '\n':
+                    args[-1][:-1]
+                total_size += int(args[-1])
+            except:
                 pass
             count += 1
             if count % 10 == 0:
-                print("Total size: {}".format(total_size))
-                for code in sorted(status_counts.keys()):
-                    if status_counts[code] != 0:
-                        print("{}: {}".format(code, status_counts[code]))
-        print("Total size: {}".format(total_size))
-        for code in sorted(status_counts.keys()):
-            if status_counts[code] != 0:
-                print("{}: {}".format(code, status_counts[code]))
-    except KeyboardInterrupt:
-        print("Total size: {}".format(total_size))
-        for code in sorted(status_counts.keys()):
-            if status_counts[code] != 0:
-                print("{}: {}".format(code, status_counts[code]))
+                print("File size: {}".format(total_size))
+                for status in sorted(status_codes.keys()):
+                    if status_codes[status] != 0:
+                        print("{}: {}".format(
+                            status, status_codes[status]))
+        print("File size: {}".format(total_size))
+        for status in sorted(status_codes.keys()):
+            if status_codes[status] != 0:
+                print("{}: {}".format(status, status_codes[status]))
+    except KeyboardInterrupt as err:
+        print("File size: {}".format(total_size))
+        for status in sorted(status_codes.keys()):
+            if status_codes[status] != 0:
+                print("{}: {}".format(status, status_codes[status]))
         raise
