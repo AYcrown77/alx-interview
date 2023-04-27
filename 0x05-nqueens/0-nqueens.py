b6a9717b6db46a0d5bf1885e1065cabbfa5f8bec
AYcrown77 <aycrown77@gmail.com>
2023-04-27 12:31:56 +0100

    Solve task 0

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Module for the N queens puzzle
+"""
+
+import sys
+
+
+def main():
+    """
+    Function that implements all possible solutions to
+    the n queens problem
+    """
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        no = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+
+    if no < 4:
+        print('N must be at least 4')
+        sys.exit(1)
+
+    alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    board = {}
+    for i in range(1, no + 1):
+        board[i] = alph[i - 1]
+
+    return []
+
+if __name__ == '__main__':
+    main()
