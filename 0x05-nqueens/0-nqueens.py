27c1243701f8282570393d15562bda235e3c4e9c
AYcrown77 <aycrown77@gmail.com>
2023-04-28 08:35:27 +0100

    Solve task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,56 @@
 #!/usr/bin/python3
-"""A program to solve the N Queens puzzle on an NxN chessboard"""
-
-
 import sys
 
 
-def find_solutions(row, column):
-    """Recursively generates all possible N Queens solutions"""
-    result = [[]]
-    for queen in range(row):
-        result = place_queen(queen, column, result)
-    return result
+def nqueens(n: int):
+    """
+    backtracking
+    """
+    def backtrack(row, queens, diagonals, anti_diagonals, board):
+        if row == n:
+            # found a valid solution
+            print([[i, j] for i, j in queens])
+            return
+
+        for col in range(n):
+            # check if it is safe to place a queen in this position
+            if col not in board and row - \
+                    col not in diagonals and row + col not in anti_diagonals:
+                board.add(col)
+                diagonals.add(row - col)
+                anti_diagonals.add(row + col)
+                queens.append((row, col))
+
+                backtrack(row + 1, queens, diagonals, anti_diagonals, board)
+
+                board.remove(col)
+                diagonals.remove(row - col)
+                anti_diagonals.remove(row + col)
+                queens.pop()
+
+    board = set()
+    queens = []
+    diagonals = set()
+    anti_diagonals = set()
+
+    backtrack(0, queens, diagonals, anti_diagonals, board)
+
+    if not queens:
+        print("No solution found for {}-queens.".format(n))
 
 
-def place_queen(queen, column, previous_result):
-    """Places a queen on the next column and returns all safe positions"""
-    safe_positions = []
-    for solution in previous_result:
-        for x in range(column):
-            if is_safe(queen, x, solution):
-                safe_positions.append(solution + [x])
-    return safe_positions
-
-
-def is_safe(q, x, solution):
-    """Checks if a queen can be safely placed at the given position"""
-    if x in solution:
-        return False
-    else:
-        return all(abs(solution[column] - x) != q - column
-                   for column in range(q))
-
-
-def initialize():
-    """Parses the command-line argument and checks its validity"""
+if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-    if sys.argv[1].isdigit():
+        exit(1)
+
+    try:
         n = int(sys.argv[1])
-    else:
+        if n < 4:
+            print("N must be at least 4")
+            exit(1)
+    except ValueError:
         print("N must be a number")
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-    return n
+        exit(1)
 
-
-def solve_n_queens():
-    """Solves the N Queens problem and prints all solutions"""
-    n = initialize()
-    solutions = find_solutions(n, n)
-    for solution in solutions:
-        cleaned_solution = []
-        for row, column in enumerate(solution):
-            cleaned_solution.append([row, column])
-        print(cleaned_solution)
-
-
-if __name__ == '__main__':
-    solve_n_queens()
+    nqueens(n)
