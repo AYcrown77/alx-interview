cd4c026a6f5f743f543bca5c3a65baca71a83968
AYcrown77 <aycrown77@gmail.com>
2023-04-28 08:12:03 +0100

    Solve task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -16,18 +16,18 @@ def main():
         sys.exit(1)
 
     try:
-        no = int(sys.argv[1])
+        n = int(sys.argv[1])
     except ValueError:
         print('N must be a number')
         sys.exit(1)
 
-    if no < 4:
+    if n < 4:
         print('N must be at least 4')
         sys.exit(1)
 
     alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     board = {}
-    for i in range(1, no + 1):
+    for i in range(1, n + 1):
         board[i] = alph[i - 1]
 
     return []
