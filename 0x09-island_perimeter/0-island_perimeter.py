3a850ad0bdeebad2dc18e6fb89bc1e6f147e9e26
AYcrown77 <aycrown77@gmail.com>
2023-06-19 17:26:59 +0100

    Solve the task

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Module for island perimeter problem """
+""" Module of island perimeter problem """
 
 
 def island_perimeter(grid):
-    """ Returns the perimeter of the island described in grid """
+    """ Returns the perimeter of the island """
 
     if not grid:
         return 0
-    perim = 0
+    perimeter = 0
     for row in range(len(grid)):
         for col in range(len(grid[row])):
             if grid[row][col] == 1:
                 if row == 0 or grid[row - 1][col] == 0:
-                    perim += 1
+                    perimeter += 1
                 if col == 0 or grid[row][col - 1] == 0:
-                    perim += 1
+                    perimeter += 1
                 if row == len(grid) - 1 or grid[row + 1][col] == 0:
-                    perim += 1
+                    perimeter += 1
                 if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
-                    perim += 1
-    return perim
+                    perimeter += 1
+    return perimeter
